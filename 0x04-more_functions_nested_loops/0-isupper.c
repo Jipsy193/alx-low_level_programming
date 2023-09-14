4ae787ec9ef1d2892085ef719761e327e6258651
Joy Oscar <joychidi02@gmail.com>
2023-09-14 13:28:59 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h'
+/**
+* isupper - checks for uppercase character.
+*
+* Return: 1 if c is 0
+*/
+int _isupper(int c)
+{
+int start = 65, end = 90;
+if (>= start && c <= end)
+return(1);
+return (0);
+}
