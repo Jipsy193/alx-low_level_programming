392dba72078f39bd141f14071291f26b781f7e70
Joy Oscar <joychidi02@gmail.com>
2023-09-28 17:20:55 -0700

    100-is_palindrome.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,17 +4,26 @@ int is_prime_helper(int n, int divisor)
 /**
 * is_prime_helper - says if an integer is a prime number or not
 * @n: number to evaluate
-*@divisor: check if n is divisible by other number than 1 and itself
+*
 * Return: 1 if n is a prime number, 0 if not
 */
 {
- if (divisor <= 1) return 1;
- if (n % divisor == 0) return 0;
-return is_prime_helper(n, divisor - 1);
+if (divisor <= 1)
+return 1;
+if (n % divisor == 0)
+return 0;
+return (is_prime_helper(n, divisor - 1));
 }
 int is_prime_number(int n)
+/**
+* Is_prime_number- calculates if a number is prime recursively
+* @n: number to evaluate
+* @divisor: iterator
+*
+* Return: 1 if n is prime, 0 if not
+*/
 {
 if (n <= 1) return 0;
- return is_prime_helper(n, n - 1);
+return (is_prime_helper(n, n - 1));
 }
 
